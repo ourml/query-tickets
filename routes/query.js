1500ba70c2ced471aa7c733f1f24dc30607d1027
Charlene Vickery <uzybxw@163.com>
2017-12-24 09:18:29 +0000

    single trip finished.

--- routes/query.js
+++ routes/query.js
@@ -4,6 +4,9 @@ const request = require('request');
 
 let StationName = require('../models/station-name.model')
 
+// https://train.qunar.com/dict/open/s2s.do?callback=jQuery17208363141274333181_1514044478877&dptStation=%E5%8C%97%E4%BA%AC&arrStation=%E5%B9%BF%E5%B7%9E&date=2017-12-25&type=normal&user=neibu&source=site&start=1&num=500&sort=3&_=1514044479207
+// https://train.qunar.com/dict/open/s2s.do?callback=json&dptStation=%E5%8C%97%E4%BA%AC&arrStation=%E5%B9%BF%E5%B7%9E&date=2017-12-25&type=normal&user=neibu&source=site&start=1&num=500&sort=3&_=1514044479207
+
 /* GET query listing. */
 router.get('/', function(req, res, next) {
   res.render('query', {
@@ -18,113 +21,389 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:code/:date/:from/:to', (req, res, next) => {
-  let code = req.params.code
-  res.render('query', {
-    title: '火车票查询信息',
-    from: '',
-    to: '',
-    date: '',
-    ticketStatus: '',
-    dataType: 'detail',
-    data: ''
-  });
-  console.log(code)
+  const date = req.params.date
+  const code = req.params.code
+  const from = req.params.from
+  const to = req.params.to
+
+  // console.log(`code: ${code}`)
+  // console.log(`date: ${date}`)
+  // console.log(`from: ${from}`)
+  // console.log(`to: ${to}`)
+
+  const apiURL = 'https://train.qunar.com/dict/open/s2s.do?callback=json'
+  const fromStation = '&dptStation=' + from
+  const toStation = '&arrStation=' + to
+  const qdate = '&date=' + date
+  const restMsg = '&type=normal&user=neibu&source=site&start=1&num=500&sort=3&_=1514044479207'
+  const originURL = apiURL + fromStation + toStation + qdate + restMsg
+  const encodeurl = encodeURI(originURL)
+
+  request.get(encodeurl, (err, response, body) => {
+    err && console.log(err)
+    try {
+      const result = JSON.parse(body.slice(9, -2))
+      if (result.ret && result.errmsg === '请求成功' && result.errcode === 0) {
+        const data = result.data
+        let postData = []
+        if (data.flag && data.errorCode === 0) {
+          const s2sBeanList = data.s2sBeanList
+          s2sBeanList.forEach(element => {
+            const trainNo = element.trainNo
+            const seats = element.seats
+            const sStation = element.startStationName
+            const eStation = element.endStationName
+            const dptTime = element.dptTime
+            const arrTime = element.arrTime
+
+            const extraBeanMap = element.extraBeanMap
+            const arrDate = extraBeanMap.arrDate
+            const intervalMiles = extraBeanMap.intervalMiles
+            const interval = extraBeanMap.interval
+            const trainType = extraBeanMap.trainType
+            if (trainNo === code) {
+              // console.log(`aaa`)
+              postData.push({
+                seats: seats,
+                sstation: sStation,
+                estation: eStation,
+                dpttime: dptTime,
+                arrtime: arrTime,
+                arrdate: arrDate,
+                intervalmiles: intervalMiles,
+                interval: interval,
+                traintype: trainType
+              })
+            }
+            // console.log(arrDate)
+            // console.log(intervalMiles)
+            // console.log(interval)
+            // console.log(trainType)
+          })
+          // res.send(postData)
+          res.render('query', {
+            title: '车次车票详情',
+            from: from,
+            to: to,
+            date: date,
+            code: code,
+            dataType: 'detail',
+            data: postData
+          })
+          // console.log(postData)
+        }
+        else {
+          console.log(`error. 5`)
+          res.send(`error has occurred. 5`)
+        }
+        // console.log(result)
+      }
+      else {
+        console.log(`获取出错. 4`)
+        res.send(`error has occurred. 4`)
+      }
+    }
+    catch (e) {
+      console.log(e)
+      res.send('error has occurred. 6')
+    }
+
+    // console.log(result1.slice(9, -2))
+  })
+
+  // console.log(encodeurl)
+  // console.log(fromStation)
+  // console.log(toStation)
+  // console.log(qdate)
+
 })
 
 router.post('/', function(req, res, next) {
+
+  // let fromStation = req.body.fromStation
+  // let toStation = req.body.toStation
+  // let date = req.body.date
+  // let outDate = req.body.outDate
+  // let backDate = req.body.backDate
+  // let ticketType = req.body.ticketType
+  // let trainType = req.body.trainType
+
+  // ticketType === 'ADULT' ? ticketType = 'ADULT' : ticketType = 'ADULT'
+
+  // function query(date, fromStation, toStation, ticketType, callback) {
+  //   let query = '?leftTicketDTO.train_date=' + date
+  //   if (fromStation) {
+  //     StationName
+  //       .findOne({ name: fromStation })
+  //       .exec((err, result) => {
+  //         err && console.log(err)
+  //         query += '&leftTicketDTO.from_station=' + result.aliascode
+  //       })
+  //   }
+  //   if (toStation) {
+  //     StationName
+  //       .findOne({ name: toStation })
+  //       .exec((err, result) => {
+  //         err && callback(null, err)
+  //         query += '&leftTicketDTO.to_station=' + result.aliascode + '&purpose_codes=' + ticketType
+  //         callback(null, query)
+  //       })
+  //   }
+  // }
+  // query(date, fromStation, toStation, ticketType, (err, query) => {
+  //   err && console.log(err)
+  //   let apiURL = 'https://kyfw.12306.cn/otn/leftTicket/query'
+  //   let originURL = apiURL + query
+
+  //   request.get(originURL, (err, response, body) => {
+  //     err && console.log(err)
+
+  //     let fresult = JSON.parse(body)
+  //     let status = fresult.status
+  //     let httpstatus = fresult.httpstatus
+  //     let messages = fresult.messages
+  //     // let validateMessages = fresult.validateMessages
+  //     let data = fresult.data
+
+  //     if (status && httpstatus === 200 && data) {
+  //       let result = data.result
+  //       let flag = data.flag
+  //       let map = data.map
+  //       let msg = []
+  //       let gotData = []
+  //       let ticketStatus = 0
+
+  //       if (result) {
+  //         result.forEach(element => {
+  //           let msg = element.split('|')
+  //           let tmpTime = []
+  //           msg.forEach(item => {
+  //             if (item.includes(':')) {
+  //               tmpTime.push(item)
+  //             }
+  //           })
+  //           if (msg.includes('有')) {
+  //             ticketStatus = 1
+  //           }
+  //           gotData.push({
+  //             code: msg[3],
+  //             times: tmpTime
+  //           })
+  //         })
+  //         res.render('query', {
+  //           title: '车票详情',
+  //           from: req.body.fromStation,
+  //           to: req.body.toStation,
+  //           date: date ? date : outDate,
+  //           ticketStatus: ticketStatus,
+  //           dataType: 'ticket',
+  //           data: gotData
+  //         })
+  //       }
+  //       else {
+  //         res.send('error has occurred.')
+  //         console.log(`暂无此信息`)
+  //       }
+  //     }
+  //     else {
+  //       res.send('error has occurred.')
+  //       console.log(messages)
+  //     }
+  //   })
+  // })
+  // if (!fromStation) {
+  //   res.redirect('/')
+  // }
+
   let fromStation = req.body.fromStation
   let toStation = req.body.toStation
   let date = req.body.date
-  let outDate = req.body.outDate
-  let inDate = req.body.inDate
-  let ticketType = req.body.ticketType
+  let backDate = req.body.backDate
+
+  // let ticketType = req.body.ticketType
+  // ticketType === 'ADULT' ? ticketType = 'ADULT' : ticketType = 'ADULT'
+  // ticketType === 'STU' ? ticketType = 'ADULT' : ticketType = 'ADULT'
+
+  let ticketType = 'ADULT'
   let trainType = req.body.trainType
 
-  ticketType === 'adult' ? ticketType = 'ADULT' : ticketType = 'ADULT'
 
-  function query(date, fromStation, toStation, ticketType, callback) {
-    let query = '?leftTicketDTO.train_date=' + date
-    if (fromStation) {
+  if (backDate) {
+    console.log(`往返票`)
+    console.log(`fromStation: ${fromStation}`)
+    console.log(`toStation: ${toStation}`)
+    console.log(`date: ${date}`)
+    console.log(`backDate: ${backDate}`)
+    console.log(`ticketType: ${ticketType}`)
+    console.log(`trainType: ${trainType}`)
+    const query = (date, fromStation, toStation, ticketType) => {
+      let query = '?leftTicketDTO.train_date=' + date
+      return query
+    }
+
+    console.log(query(date, fromStation, toStation, ticketType))
+  }
+  else {
+    // console.log(`单程票`)
+    // console.log(`fromStation: ${fromStation}`)
+    // console.log(`toStation: ${toStation}`)
+    // console.log(`date: ${date}`)
+    // console.log(`ticketType: ${ticketType}`)
+    // console.log(`trainType: ${trainType}`)
+
+    const query = (date, fromStation, toStation, ticketType, callback) => {
+      let query = '?leftTicketDTO.train_date=' + date
       StationName
         .findOne({ name: fromStation })
         .exec((err, result) => {
-          err && console.log(err)
-          query += '&leftTicketDTO.from_station=' + result.aliascode
+          err && callback(null, err)
+          if (result) {
+            query += '&leftTicketDTO.from_station=' + result.aliascode
+          }
+          else {
+            console.log(`无法获取相关数据`)
+          }
         })
-    }
-    if (toStation) {
       StationName
         .findOne({ name: toStation })
         .exec((err, result) => {
           err && callback(null, err)
-          query += '&leftTicketDTO.to_station=' + result.aliascode + '&purpose_codes=' + ticketType
-          callback(null, query)
+          if (result) {
+            query += '&leftTicketDTO.to_station=' + result.aliascode + '&purpose_codes=' + ticketType
+            callback(null, query)
+          }
+          else {
+            callback(null, err)
+            console.log(`无法获取相关数据`)
+          }
         })
     }
-  }
-  query(date, fromStation, toStation, ticketType, (err, query) => {
-    err && console.log(err)
-    let apiURL = 'https://kyfw.12306.cn/otn/leftTicket/query'
-    let originURL = apiURL + query
 
-    request.get(originURL, (err, response, body) => {
+    query(date, fromStation, toStation, ticketType, (err, result) => {
       err && console.log(err)
+      // console.log(result)
 
-      let fresult = JSON.parse(body)
-      let status = fresult.status
-      let httpstatus = fresult.httpstatus
-      let messages = fresult.messages
-      // let validateMessages = fresult.validateMessages
-      let data = fresult.data
+      const apiURL = 'https://kyfw.12306.cn/otn/leftTicket/query'
+      // const apiURL = 'https://www.baidu.com'
+      const originURL = apiURL + result
 
-      if (status && httpstatus === 200 && data) {
-        let result = data.result
-        let flag = data.flag
-        let map = data.map
-        let msg = []
-        let gotData = []
-        let ticketStatus = 0
+      request.get(originURL, (err, response, body) => {
+        err && console.log(err)
+        try {
+          let fresult = JSON.parse(body)
+          let status = fresult.status
+          let httpstatus = fresult.httpstatus
+          let messages = fresult.messages
+          let data = fresult.data
 
-        if (result) {
-          result.forEach(element => {
-            let msg = element.split('|')
-            let tmpTime = []
-            msg.forEach(item => {
-              if (item.includes(':')) {
-                tmpTime.push(item)
-              }
-            })
-            if (msg.includes('有')) {
-              ticketStatus = 1
+          if (status && httpstatus === 200 && data) {
+            let result = data.result
+            let flag = data.flag
+            let map = data.map
+            let msg = []
+            let gotData = []
+            let ticketStatus = 0
+
+            if (result) {
+              result.forEach(element => {
+                let msg = element.split('|')
+                let tmpTime = []
+                msg.forEach(item => {
+                  if (item.includes(':')) {
+                    tmpTime.push(item)
+                  }
+                })
+                if (msg.includes('有')) {
+                  ticketStatus = 1
+                }
+                gotData.push({
+                  code: msg[3],
+                  times: tmpTime
+                })
+              })
+              res.render('query', {
+                title: '车票详情',
+                from: req.body.fromStation,
+                to: req.body.toStation,
+                date: date,
+                ticketStatus: ticketStatus,
+                dataType: 'ticket',
+                data: gotData
+              })
             }
-            gotData.push({
-              code: msg[3],
-              times: tmpTime
-            })
-          })
-          res.render('query', {
-            title: '车票详情',
-            from: req.body.fromStation,
-            to: req.body.toStation,
-            date: date ? date : outDate,
-            ticketStatus: ticketStatus,
-            dataType: 'ticket',
-            data: gotData
-          })
+            else {
+              res.send('error has occurred.1')
+              console.log(`暂无此信息`)
+            }
+          }
+          else {
+            res.send('error has occurred.2')
+            console.log(messages)
+          }
         }
-        else {
-          res.send('error has occurred.')
-          console.log(`暂无此信息`)
+        catch (e) {
+          res.send('errors has occurred. 3')
+          console.log(e)
         }
-      }
-      else {
-        res.send('error has occurred.')
-        console.log(messages)
-      }
+
+        // if (JSON.parse(body)) {
+        //   let fresult = JSON.parse(body)
+        //   let status = fresult.status
+        //   let httpstatus = fresult.httpstatus
+        //   let messages = fresult.messages
+        //   let data = fresult.data
+
+        //   if (status && httpstatus === 200 && data) {
+        //     let result = data.result
+        //     let flag = data.flag
+        //     let map = data.map
+        //     let msg = []
+        //     let gotData = []
+        //     let ticketStatus = 0
+
+        //     if (result) {
+        //       result.forEach(element => {
+        //         let msg = element.split('|')
+        //         let tmpTime = []
+        //         msg.forEach(item => {
+        //           if (item.includes(':')) {
+        //             tmpTime.push(item)
+        //           }
+        //         })
+        //         if (msg.includes('有')) {
+        //           ticketStatus = 1
+        //         }
+        //         gotData.push({
+        //           code: msg[3],
+        //           times: tmpTime
+        //         })
+        //       })
+        //       res.render('query', {
+        //         title: '车票详情',
+        //         from: req.body.fromStation,
+        //         to: req.body.toStation,
+        //         date: date,
+        //         ticketStatus: ticketStatus,
+        //         dataType: 'ticket',
+        //         data: gotData
+        //       })
+        //     }
+        //     else {
+        //       res.send('error has occurred.1')
+        //       console.log(`暂无此信息`)
+        //     }
+        //   }
+        //   else {
+        //     res.send('error has occurred.2')
+        //     console.log(messages)
+        //   }
+        // }
+        // else {
+        //   console.log(`资源获取出错`)
+        //   res.send(`资源获取出错`)
+        // }
+      })
     })
-  })
-  if (!fromStation) {
-    res.redirect('/')
   }
 })
 
